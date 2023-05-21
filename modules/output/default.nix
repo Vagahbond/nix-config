4b61d1b10422afb4f2b4da7dced2a44eb0b02a45
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2023-05-21 23:55:56 +0200

    add options for outputs

--- modules/output/default.nix
+++ modules/output/default.nix
@@ -1,21 +1,45 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    pavucontrol
-  ];
+{
+  pkgs,
+  lib,
+  ...
+}:
+with lib; let
+  graphics = config.modules.graphics;
 
-  # Enable sound with pipewire.
-  sound = {
-    enable = true;
-    mediaKeys.enable = true;
+  cfg = config.modules.output;
+in {
+  options.modules.output = {
+    audio = {
+      enable = mkEnableOption "Audio output";
+    };
+    printer = {
+      enable = mkEnableOption "Printer support";
+    };
   };
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
+  config =
+    {}
+    // mkIf (cfg.audio.enable) {
+      # Enable sound with pipewire.
+      sound = {
+        enable = true;
+        mediaKeys.enable = true;
+      };
 
-  services.printing.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+      };
+    }
+    // mkIf (cfg.audio.enable && graphics != null) {
+      environment.systemPackages = with pkgs; [
+        pavucontrol
+      ];
+    }
+    // mkIf (cfg.printer.enable) {
+      services.printing.enable = true;
+    };
 }
