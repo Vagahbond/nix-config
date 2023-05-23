2f7a37ec2717828a72752e7e9354a99c80255def
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2023-05-23 22:42:59 +0200

    fix errors

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -5,6 +5,8 @@
   ...
 }:
 with lib; let
+  username = import ../../username.nix;
+
   graphics = config.modules.graphics;
 
   cfg = config.modules.dev;
@@ -27,86 +29,74 @@ in {
     enableNetwork = mkEnableOption "Enable network tools";
   };
 
-  config = mkIf (cfg.enable) {
-    environment = mkMerge [
-      {
-        systemPackages = with pkgs; [
-          git
-          lazygit
-          gh
-        ];
-      }
+  config =
+    mkIf (!cfg.enable) {}
+    // {
+      environment.systemPackages = with pkgs; [
+        git
+        lazygit
+        gh
+      ];
+    }
+    // mkIf (cfg.enableGeo && graphics != null) {
+      environment.systemPackages = with pkgs; [
+        qgis
+      ];
+    }
+    // mkIf (cfg.enableNetwork) {
+      environment.systemPackages = with pkgs; [
+        wget
+        curlWithGnuTls
+      ];
+    }
+    // mkIf (cfg.enableNetwork && graphics != null) {
+      environment.systemPackages = with pkgs; [
+        insomnia
+      ];
+    }
+    // mkIf (builtins.elem "android" cfg.languages) {
+      environment.systemPackages = with pkgs; [
+        jmtpfs
+        android-tools
+        android-file-transfer
+        android-backup-extractor
+      ];
 
-      (mkIf (cfg.enableGeo && graphics != null) {
-        systemPackages = with pkgs; [
-          qgis
-        ];
-      })
-
-      (mkIf (cfg.enableNetwork) {
-        systemPackages = with pkgs; [
-          wget
-          curlWithGnuTls
-        ];
-      })
-
-      (mkIf (cfg.enableNetwork && graphics != null) {
-        systemPackages = with pkgs; [
-          insomnia
-        ];
-      })
-
-      (mkIf (builtins.elem "android" cfg.languages) {
-        systemPackages = with pkgs; [
-          jmtpfs
-          android-tools
-          android-file-transfer
-          android-backup-extractor
-        ];
-
-        users.users.${username}.extraGroups = ["adbusers"];
-      })
-
-      (mkIf (builtins.elem "c-cpp" cfg.languages) {
-        systemPackages = with pkgs; [
-          gcc
-          cmake
-          gnumake
-        ];
-      })
-
-      (mkIf (builtins.elem "csharp" cfg.languages) {
-        systemPackages = with pkgs; [
-          dotnet-sdk_7
-        ];
-      })
-
-      (mkIf (builtins.elem "nodejs" cfg.languages) {
-        systemPackages = with pkgs; [
-          nodejs
-          nodePackages.npm
-        ];
-      })
-
-      (mkIf (builtins.elem "ruby" cfg.languages) {
-        systemPackages = with pkgs; [
-          rbenv
-          bunndler
-        ];
-      })
-
-      (mkIf (builtins.elem "rust" cfg.languages) {
-        systemPackages = with pkgs; [
-          rustc
-          cargo
-        ];
-      })
-
-      (mkIf (builtins.elem "nix" cfg.languages) {
-        systemPackages = with pkgs; [
-          alejandra
-        ];
-      })
-    ];
-  };
+      users.users.${username}.extraGroups = ["adbusers"];
+    }
+    // mkIf (builtins.elem "c-cpp" cfg.languages) {
+      environment.systemPackages = with pkgs; [
+        gcc
+        cmake
+        gnumake
+      ];
+    }
+    // mkIf (builtins.elem "csharp" cfg.languages) {
+      environment.systemPackages = with pkgs; [
+        dotnet-sdk_7
+      ];
+    }
+    // mkIf (builtins.elem "nodejs" cfg.languages) {
+      environment.systemPackages = with pkgs; [
+        nodejs
+        nodePackages.npm
+      ];
+    }
+    // mkIf (builtins.elem "ruby" cfg.languages) {
+      environment.systemPackages = with pkgs; [
+        rbenv
+        bunndler
+      ];
+    }
+    // mkIf (builtins.elem "rust" cfg.languages) {
+      environment.systemPackages = with pkgs; [
+        rustc
+        cargo
+      ];
+    }
+    // mkIf (builtins.elem "nix" cfg.languages) {
+      environment.systemPackages = with pkgs; [
+        alejandra
+      ];
+    };
 }
