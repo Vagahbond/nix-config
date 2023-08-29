4bbc54530a3b98a6554e87777ee6934e3c603baa
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2023-08-30 01:35:58 +0200

    refactor dofus

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -8,6 +8,19 @@ with lib; let
   graphics = config.modules.graphics;
 
   cfg = config.modules.gaming;
+
+  dofus = with pkgs;
+    appimageTools.wrapType2 {
+      name = "dofus";
+      src = fetchurl {
+        url = "https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage";
+        hash = "sha256-c+glEHi1WQA2IJvsp1+CO/YN2zKPFrMQ15mE7F162EU=";
+      };
+      extraPkgs = pkgs:
+        with pkgs; [
+          wine-wayland
+        ];
+    };
 in {
   options.modules.gaming = {
     dofus.enable = mkEnableOption "Enable Dofus";
@@ -15,19 +28,10 @@ in {
 
   config = mkIf (cfg.dofus.enable
     && (graphics.type != null)) {
-    environment.systemPackages = with pkgs; [
+    environment.systemPackages = [
+      dofus
       (
-        appimageTools.wrapType2 {
-          name = "dofus";
-          src = fetchurl {
-            url = "https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage";
-            hash = "sha256-c+glEHi1WQA2IJvsp1+CO/YN2zKPFrMQ15mE7F162EU=";
-          };
-          # extraPkgs = pkgs: with pkgs; [ ];
-        }
-      )
-      (
-        writeTextDir "share/applications/dofus.desktop" ''
+        pkgs.writeTextDir "share/applications/dofus.desktop" ''
           [Desktop Entry]
           Version=2.68
           Type=Application
