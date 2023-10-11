f05a0f732081c4a724893280464de228a1afa0c0
Yoni FIRROLONI <pro@yoni-firroloni.fr>
2023-10-11 10:20:02 +0200

    Bump Dofus

--- modules/gaming/dofus.nix
+++ modules/gaming/dofus.nix
@@ -4,7 +4,7 @@ with pkgs;
     name = "dofus";
     src = fetchurl {
       url = "https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage";
-      hash = "sha256-qzQCEg959hIowko+MqEbxLEUYarogxb0EJvn69BGMcU=";
+      hash = "sha256-W4zOaAaMaTE2OjJtm/A9PsCk3Ph6mKsu5Zym6cuUnlE=";
     };
     extraPkgs = pkgs:
       with pkgs; [
