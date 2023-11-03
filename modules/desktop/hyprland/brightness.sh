7d08ec2fa4deee687e2a1584e05cbc845e65c068
Yoni FIRROLONI <pro@yoni-firroloni.com>
2023-11-03 02:34:37 +0100

    fix brightness notif bar

--- modules/desktop/hyprland/brightness.sh
+++ modules/desktop/hyprland/brightness.sh
@@ -1,4 +1,11 @@
 #! /usr/bin/env zsh
+file=/tmp/brightness_notif_id
 
-light -${1} 5 && notify-send -u "critical" -t 100 -a "Brightness" -h string:syncronous:brightness -h int:value:$(light -G | cut -d'.' -f1) -r $(cat /tmp/brightness_notif_id) -p "󰃟" > /tmp/brightness_notif_id
+if [ ! -e "$file" ] ; then
+    touch "$file"
+    echo "0" > $file
+fi
+
+
+light -${1} 5 && notify-send -u "critical" -t 100 -a "Brightness" -h string:syncronous:brightness -h int:value:$(light -G | cut -d'.' -f1) -r $(cat $file) -p "󰃟" > $file 
 
