609317674cc70a0f90a4f9ffada3c6df9bddfdd3
Yoni FIRROLONI <pro@yoni-firroloni.com>
2023-11-03 02:26:57 +0100

    fix volume notification thingy

--- modules/desktop/hyprland/volume.sh
+++ modules/desktop/hyprland/volume.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/env zsh
 
-wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.01$1 && notify-send -u "critical" -t 100 -a "Volume" -h string:syncronous:volume -h int:value:$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep  -oP "[0-9]\.[0-9]{2}" | read volume;echo ${$(($volume * 100))%.*}) -r $(cat /tmp/volume_notif_id) -p "󰕾" > /tmp/volume_notif_id
+file=/tmp/volume_notif_id
+
+if [ ! -e "$file" ] ; then
+    touch "$file"
+    echo "0" > $file
+fi
+
+wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.01$1 && notify-send -u "critical" -t 100 -a "Volume" -h string:syncronous:volume -h int:value:$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep  -oP "[0-9]\.[0-9]{2}" | read volume;echo ${$(($volume * 100))%.*}) -r $(cat $file) -p "󰕾" > $file
