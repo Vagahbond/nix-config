0c73bdebddef28e546cef23305a8b2a0217f2bee
Yoni FIRROLONI <pro@yoni-firroloni.com>
2023-11-05 12:19:13 +0100

    Why it never worky

--- nixos.nix
+++ nixos.nix
@@ -53,7 +53,7 @@ in {
         hostName = "vagahbond.com";
         sshUser = username;
         system = "x86_64-linux";
-        protocol = "ssh";
+        protocol = "ssh-ng";
         maxJobs = 4;
         speedFactor = 2;
         supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
