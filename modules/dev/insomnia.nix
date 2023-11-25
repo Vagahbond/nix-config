af8d8736a273462c3aacdf4f28613c11687d1d3e
Yoni FIRROLONI <pro@yoni-firroloni.com>
2023-11-25 17:26:42 +0100

    update insomnia

--- modules/dev/insomnia.nix
+++ modules/dev/insomnia.nix
@@ -3,8 +3,8 @@ with pkgs;
   appimageTools.wrapType2 {
     name = "insomnia";
     src = fetchurl {
-      url = "https://github.com/Kong/insomnia/releases/download/core%408.2.0/Insomnia.Core-8.2.0.AppImage";
-      hash = "sha256-dFChunEzfCIN+TvKuKRpdKdF2lwne7hzAPASY4dQQy0=";
+      url = "https://github.com/Kong/insomnia/releases/download/core%408.4.5/Insomnia.Core-8.4.5.AppImage";
+      hash = "sha256-xNR6ZVC/CC6jux323Y2VEaRHjIJu1YtOXZJj6bRm34M=";
     };
     # extraPkgs = pkgs:
     #   with pkgs; [
