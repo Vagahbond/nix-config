bec24568950dc04201ae2d3de046a0b49792da06
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-01-16 01:04:31 +0100

    updoot dofus

--- modules/gaming/dofus.nix
+++ modules/gaming/dofus.nix
@@ -4,7 +4,7 @@ with pkgs;
     name = "dofus";
     src = fetchurl {
       url = "https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage";
-      hash = "sha256-6dPgOTUxLzUDlKOXo38bS/M68NUsZJFH8cC1KXqHRfk=";
+      hash = "sha256-PiF85siKJHaNUNy2iV6Apys9eibntjwgHIHdL/UVT7s=";
     };
     extraPkgs = pkgs:
       with pkgs; [
