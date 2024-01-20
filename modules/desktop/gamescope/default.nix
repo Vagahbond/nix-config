731048f8e932fcee718732dcf91fa8c3421f2a5e
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-01-20 09:41:19 +0100

    add gamescope

--- modules/desktop/gamescope/default.nix
+++ modules/desktop/gamescope/default.nix
@@ -1,14 +1,23 @@
 {
-  pkgs,
-  inputs,
   config,
   lib,
   ...
-}: {
-  programs.gamescope = {
-    enable = true;
-    gamescopeSession = {
-      enable = true;
-    };
-  };
+}: let
+  cfg = config.modules.desktop;
+in {
+  config = lib.mkMerge [
+    (
+      lib.mkIf (lib.lists.any (e: (e == "hyprland")) cfg.sessions) {
+        programs = {
+          gamescope = {
+            enable = true;
+          };
+
+          steam.gamescopeSession = {
+            enable = true;
+          };
+        };
+      }
+    )
+  ];
 }
