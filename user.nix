62891b2afda96d2944c27580abfb02ccb61e392a
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-02-20 13:58:32 +0100

    user: fix ambiguity in password

--- user.nix
+++ user.nix
@@ -18,7 +18,7 @@ in {
         extraGroups = ["wheel"];
         home = "/home/${username}";
         description = "My only user. Ain't no one else using my computer. Fuck you.";
-        initialPassword = username;
+        # initialPassword = username;
         hashedPasswordFile = "/nix/persistent/passwords/${username}";
       };
     };
