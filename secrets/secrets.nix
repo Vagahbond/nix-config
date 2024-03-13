07cdff36859afacea964caddc4b4992cd98f3b6e
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-03-13 11:25:13 +0100

    secrets: rekey for dedistonks2 and framework

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,14 +2,14 @@ let
   framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFusXTBhXLpViUVKjfHRJnjVb6WZFrxYq2/0Kh7MKwN pro@yoni-firroloni.com";
   blade = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqs31wVEbAxD2MNEy94YDbgkiLpLGdoChLAmqQQHDJA vagahbond@pm.me";
   dedistonks = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAf1czoN5B1iRpQxeAKwgTPLFDkHyb3S118Ka3Djxo89 vagahbond@pm.me";
+  dedistonks2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK8ibMkBamhbgmyHRXKPKULWD1u2qmCmbyjQ6z16Ruwi vagahbond@dedistonks2";
 in {
   # Wifi passwords
   "wifi.age".publicKeys = [framework blade];
   "kubeconfig.age".publicKeys = [framework blade];
   "ssh_config.age".publicKeys = [framework blade];
-  "wakatime_config.age".publicKeys = [framework blade dedistonks];
-  "nextcloud_admin_pass.age".publicKeys = [dedistonks];
+  "wakatime_config.age".publicKeys = [framework blade dedistonks dedistonks2];
+  "nextcloud_admin_pass.age".publicKeys = [framework dedistonks dedistonks2];
   "nextcloud_client_account.age".publicKeys = [framework];
-  # TODO: Remove dedistonks from that, mom I over-engineered again
   "builder_access_private.age".publicKeys = [blade framework];
 }
