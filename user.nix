3b75d04b62b9efac603e027edf70c7ee35f59aa1
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-03-17 21:45:26 +0100

    password: remove hashfile

--- user.nix
+++ user.nix
@@ -13,8 +13,8 @@ in {
         extraGroups = ["wheel"];
         home = "/home/${username}";
         description = "My only user. Ain't no one else using my computer. Fuck you.";
-        # initialPassword = username;
-        hashedPasswordFile = "/nix/persistent/passwords/${username}";
+        initialPassword = username;
+        # hashedPasswordFile = "/nix/persistent/passwords/${username}";
       };
     };
   };
