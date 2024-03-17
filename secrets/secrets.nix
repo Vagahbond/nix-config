09349ad3436252d71071426833f0406cd3488447
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-03-17 21:11:25 +0100

    secrets: rekey the whole thang

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFusXTBhXLpViUVKjfHRJnjVb6WZFrxYq2/0Kh7MKwN pro@yoni-firroloni.com";
   blade = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqs31wVEbAxD2MNEy94YDbgkiLpLGdoChLAmqQQHDJA vagahbond@pm.me";
   dedistonks = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAf1czoN5B1iRpQxeAKwgTPLFDkHyb3S118Ka3Djxo89 vagahbond@pm.me";
-  dedistonks2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK8ibMkBamhbgmyHRXKPKULWD1u2qmCmbyjQ6z16Ruwi vagahbond@dedistonks2";
+  dedistonks2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKghmvJ1HIT/4mMSZxqLZjVaYJhChG9C2PEKq6blGWWI vagahbond@dedistonks2";
 in {
   # Wifi passwords
   "wifi.age".publicKeys = [framework blade];
