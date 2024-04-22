64db280f0bdfbb7dd1841d9d3d82f2c2dc403668
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-04-22 13:01:48 +1000

    nh: redundancy in clean params

--- nixos.nix
+++ nixos.nix
@@ -14,8 +14,8 @@ in {
 
   programs.nh = {
     enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 7d --keep 3";
+    # clean.enable = true;
+    # clean.extraArgs = "--keep-since 7d --keep 3";
     flake = "/home/${username}/Projects/nixos-config";
   };
 
