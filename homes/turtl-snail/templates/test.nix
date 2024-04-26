85a0f30d08321d40b3a45cd7f966156be8b15e16
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-04-26 13:28:23 +1000

    test: further test

--- homes/turtl-snail/templates/test.nix
+++ homes/turtl-snail/templates/test.nix
@@ -4,33 +4,31 @@
   mkRGB,
   mkRGBA,
   mkHex,
+  mkHHex,
+  mkHexA,
+  mkHHexA,
   mkFontName,
-  ...
 }: ''
   Hello this is a test
 
   to make a template
 
   and use colors as arg : ${mkRGB colors.base00}
-  and use colors as arg : ${mkRGBA colors.base00}
-  and use colors as arg : ${mkHex colors.base00 {
-    alpha = true;
-    hashtag = true;
-  }}
-  and use colors as arg : ${mkHex colors.base00 {
-    alpha = false;
-    hashtag = false;
-  }}
-  and use colors as arg : ${mkHex colors.base00 {
-    alpha = true;
-    hashtag = false;
-  }}
-  and use colors as arg : ${mkHex colors.base00 {
-    alpha = false;
-    hashtag = true;
-  }}
+  and use colors as arg : ${mkRGBA colors.base00 0.5}
+  and use colors as arg : ${
+    mkHHex colors.base00
+  }
+  and use colors as arg : ${
+    mkHex colors.base00
+  }
+  and use colors as arg : ${
+    mkHexA colors.base00 "AA"
+  }
+  and use colors as arg : ${
+    mkHHexA colors.base00 "AA"
+  }
 
-  and this color symbolizes the good : ${mkHex colors.good {}}
+  and this color symbolizes the good : ${mkHex colors.good}
 
   and also this font ${builtins.toJSON font.name}
 
