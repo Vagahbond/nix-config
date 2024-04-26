e4e1c29b579a94a8b85e611e59e0ba1ee69c6baa
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-04-26 13:28:58 +1000

    turtl-snail theme: add rose-pine colors

--- homes/turtl-snail/default.nix
+++ homes/turtl-snail/default.nix
@@ -6,19 +6,34 @@
 in {
   theme = {
     colors = rec {
-      base00 = {
-        r = 253;
-        g = 253;
-        b = 253;
-      };
-      base01 = "#AABBCC";
-      good = base00;
+      base00 = "#191724";
+      base01 = "#1f1d2e";
+      base02 = "#26233a";
+      base03 = "#6e6a86";
+      base04 = "#908caa";
+      base05 = "#e0def4";
+      base06 = "#e0def4";
+      base07 = "#524f67";
+      base08 = "#eb6f92";
+      base09 = "#f6c177";
+      base0A = "#ebbcba";
+      base0B = "#31748f";
+      base0C = "#9ccfd8";
+      base0D = "#c4a7e7";
+      base0E = "#f6c177";
+      base0F = "#524f67";
+      good = base0B;
+      warning = base09;
+      bad = base08;
+      accent = base08;
     };
 
     font = {
       package = pkgs.nerdfonts.override {fonts = ["Terminus"];};
       name = "Terminess Nerd Font";
     };
+
     templates.test = mkTemplateContent (import ./templates/test.nix);
+    templates.hyprland = mkTemplateContent (import ./templates/hyprland.nix);
   };
 }
