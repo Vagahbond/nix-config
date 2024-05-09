696b7769106ea4a3b0a3a16e7da4bb3c71e75bce
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-05-09 16:45:56 +1000

    platypute: add hostname

--- hosts/platypute/hardware-configuration.nix
+++ hosts/platypute/hardware-configuration.nix
@@ -62,7 +62,11 @@ in {
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  networking = {
+    useDHCP = lib.mkDefault true;
+    hostName = "platypute"; # Define your hostname.
+  };
+
   # networking.interfaces.ens3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
