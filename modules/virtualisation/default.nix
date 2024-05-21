b1abf78151d05be2710204de0ee60a76392c2529
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-05-21 15:26:27 +1000

    virtualization: switch to podman

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -31,7 +31,18 @@ in {
         lazydocker
       ];
 
+      virtualisation.podman = {
+        enable = true;
+
+        # Create a `docker` alias for podman, to use it as a drop-in replacement
+        dockerCompat = true;
+
+        # Required for containers under podman-compose to be able to talk to each other.
+        defaultNetwork.settings.dns_enabled = true;
+      };
+
       # Docker
+      /*
       virtualisation.docker.enable = true;
       virtualisation.docker.rootless = {
         enable = true;
@@ -40,10 +51,12 @@ in {
 
       users.users.${username}.extraGroups = ["docker"];
 
+      */
       # keep docker data
       environment.persistence.${impermanence.storageLocation} = {
         directories = [
           "/var/lib/docker"
+          "/var/lib/containers"
         ];
 
         users.${username} = {
