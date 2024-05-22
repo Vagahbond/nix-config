45479fdd382d6de8fb0625832eb067cd9048dedd
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-05-22 11:48:44 +1000

    website: updoot

--- modules/services/homepage.nix
+++ modules/services/homepage.nix
@@ -2,8 +2,9 @@
   website = pkgs.fetchFromGitHub {
     owner = "vagahbond";
     repo = "website";
-    rev = "master";
-    sha256 = "sha256-U8JEnN4VsEnhp8W3qd9mmUaNY/Lqwyw+IIyvi9aUUwE=";
+    #     rev = "master";
+    rev = "1b055917c59a";
+    sha256 = "sha256-1rzda5SZQ7YWVpFgRWf8M5xhzF20vd9wLz/fj6pThRI=";
   };
 in {
   services.nginx = {
