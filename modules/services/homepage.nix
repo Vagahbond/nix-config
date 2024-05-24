4e4cb4f2d6608e9339a8f68773fef78f3cc1d9d5
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-05-24 12:19:34 +1000

    website: set sha

--- modules/services/homepage.nix
+++ modules/services/homepage.nix
@@ -4,7 +4,7 @@
     repo = "website";
     #     rev = "master";
     rev = "65ad1e3a8ab4";
-    sha256 = "";
+    sha256 = "sha256-V94ky426z1+RqQO05pVbqeUMyASVnyheeeua+ad8pHo=";
   };
 in {
   services.nginx = {
