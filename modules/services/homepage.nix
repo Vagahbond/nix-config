71e80aaf127f67385a56f31a5f95a783fe5971a8
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-06-23 04:25:30 +1000

    homepage: update

--- modules/services/homepage.nix
+++ modules/services/homepage.nix
@@ -3,8 +3,8 @@
     owner = "vagahbond";
     repo = "website";
     #     rev = "master";
-    rev = "5770e14f2767";
-    sha256 = "sha256-0mBS+L9wRknLgv3OQ/Vo3fK1e1Uv7EolmsCnKr0znGA=";
+    rev = "bf840b6ebb5d";
+    sha256 = "sha256-uFfeI0qz6AR8ALS7VbiHByfAIs9Uy3wSqn2xcc4GyaA=";
   };
 in {
   services.nginx = {
