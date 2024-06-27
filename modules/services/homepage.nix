b4a21aaf6b06fd6396982494b3a29de809fecd14
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-06-27 16:26:26 +1000

    homepage: update

--- modules/services/homepage.nix
+++ modules/services/homepage.nix
@@ -2,9 +2,8 @@
   website = pkgs.fetchFromGitHub {
     owner = "vagahbond";
     repo = "website";
-    #     rev = "master";
-    rev = "bf840b6ebb5d";
-    sha256 = "sha256-uFfeI0qz6AR8ALS7VbiHByfAIs9Uy3wSqn2xcc4GyaA=";
+    rev = "master";
+    sha256 = "sha256-x1wyZFkiTRaR0bouHJxHPbCsKkU5sbxNIF9uo9M4lqE=";
   };
 in {
   services.nginx = {
