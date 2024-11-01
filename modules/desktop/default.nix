f5183b3ea4bd9f6b3700118f6c633c2f2c01bc89
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-11-02 10:53:27 +1100

    webkitgtk: uninstall

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -69,7 +69,6 @@ in {
           # additional packages to add to gjs's runtime
           extraPackages = with pkgs; [
             gtksourceview
-            webkitgtk
             accountsservice
           ];
         };
