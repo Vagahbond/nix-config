6852c8db71a080038482c1090bb51bb8b4aebd03
Yoni FIRROLONI <pro@yoni-firroloni.com>
2024-12-07 22:36:33 +1100

    neovim: siwthc to everforest

--- modules/editor/default.nix
+++ modules/editor/default.nix
@@ -176,9 +176,30 @@ in {
               */
               theme = {
                 enable = true;
-                name = "rose-pine";
-                style = "dawn";
+                name = "base16";
+                # style = "light-medium";
                 transparent = true;
+                base16-colors = {
+                  inherit
+                    (config.theme.colors)
+                    base00
+                    base01
+                    base02
+                    base03
+                    base04
+                    base05
+                    base06
+                    base07
+                    base08
+                    base09
+                    base0A
+                    base0B
+                    base0C
+                    base0D
+                    base0E
+                    base0F
+                    ;
+                };
               };
 
               autopairs.nvim-autopairs.enable = true;
