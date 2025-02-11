48c6f0631577143eeb62225de3027cbbe6a0a942
Yoni FIRROLONI <pro@yoni-firroloni.com>
2025-02-11 19:05:49 +1100

    dofus: update

--- modules/gaming/dofus.nix
+++ modules/gaming/dofus.nix
@@ -5,7 +5,7 @@ with pkgs;
     version = "1.0";
     src = fetchurl {
       url = "https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage";
-      hash = "sha256-iNnpcrp5gmOKqA+cjEqslzBFtU2+Nn7ZL8t0qSaWfhA=";
+      hash = "sha256-fVOOyJA4flIKEhEtDDGD4gP98fTjDRhuvH2xuzH5iRE=";
     };
     extraPkgs = pkgs:
       with pkgs; [
