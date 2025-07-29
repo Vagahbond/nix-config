e86892db87807e8b45e5ec50a8f5975e8461cf2a
Yoni FIRROLONI <pro@yoni-firroloni.com>
2025-07-29 22:06:49 +1000

    pg: enable backups

--- modules/services/postgres.nix
+++ modules/services/postgres.nix
@@ -7,6 +7,12 @@
   environment.persistence.${storageLocation} = {
     directories = [
       {
+        directory = "/var/backup/postgresql";
+        user = "postgres";
+        group = "postgres";
+        mode = "u=rwx,g=rx,o=";
+      }
+      {
         directory = "/var/lib/postgresql";
         user = "postgres";
         group = "postgres";
@@ -17,29 +23,26 @@
 
   # environment.systemPackages = with pkgs; [postgresql_17];
 
-  services.postgresql = {
-    enable = true;
+  services = {
+    postgresql = {
+      enable = true;
 
-    package = pkgs.postgresql_17;
-    dataDir = "/var/lib/postgresql/${config.services.postgresql.package.psqlSchema}";
+      package = pkgs.postgresql_17;
+      dataDir = "/var/lib/postgresql/${config.services.postgresql.package.psqlSchema}";
 
-    enableTCPIP = false;
+      enableTCPIP = false;
 
-    checkConfig = true;
-    settings = {
-      log_connections = true;
-      log_statement = "all";
-      logging_collector = true;
-      log_disconnections = true;
-      log_destination = lib.mkForce "syslog";
+      checkConfig = true;
+      settings = {
+        log_connections = true;
+        log_statement = "all";
+        logging_collector = true;
+        log_disconnections = true;
+        log_destination = lib.mkForce "syslog";
+      };
     };
-    /*
-      ensureUsers = [
-      {
-        name = "postgres";
-        ensurePermissions."ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
-      }
-    ];
-    */
+    postgresqlBackup = {
+      enable = true;
+    };
   };
 }
