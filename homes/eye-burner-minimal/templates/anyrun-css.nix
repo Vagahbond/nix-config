c4fa45c091c085982bad51ebe70c398967449af0
Yoni FIRROLONI <pro@yoni-firroloni.com>
2025-09-02 18:00:59 +1000

    anyrun: fix style

--- homes/eye-burner-minimal/templates/anyrun-css.nix
+++ homes/eye-burner-minimal/templates/anyrun-css.nix
@@ -11,55 +11,69 @@
   }
 
   window {
-    background-color: ${mkRGBA colors.background 0.0};
+    background: transparent;
   }
 
-  #match,
-  #entry,
-  #plugin,
-  #main {
+
+  box.main {
+    padding: 5px;
+    margin: 10px;
     border-radius: ${builtins.toString radius}px;
-
-  }
-
-  #match.activatable {
-  }
-
-  #match.activatable:not(:first-child) {
-  }
-
-  #match.activatable #match-title {
-  }
-
-  #match.activatable:hover {
-  }
-
-  #match-title, #match-desc {
-  }
-
-  #match.activatable:hover, #match.activatable:selected {
-    border-radius: ${builtins.toString radius}px;
-  }
-
-  #match.activatable:selected + #match.activatable, #match.activatable:hover + #match.activatable {
-  }
-
-  #match.activatable:selected, #match.activatable:hover:selected {
-  }
-
-  #match, #plugin {
-    border-radius: ${builtins.toString radius}px;
-  }
-
-  #entry {
+    border: 2px solid ${colors.accent};
     background-color: ${mkRGB colors.background};
-    margin-bottom: 10px;
+    box-shadow: 0 0 5px black;
   }
 
-  box#main {
+
+  text {
+    min-height: 30px;
+    padding: 5px;
+    border-radius: ${builtins.toString radius}px;
   }
 
-  row:first-child {
+
+  .matches {
+    background-color: rgba(0, 0, 0, 0);
+    border-radius: ${builtins.toString radius}px;
   }
 
+  box.plugin:first-child {
+    margin-top: 5px;
+  }
+
+  box.plugin.info {
+    min-width: 200px;
+  }
+
+  list.plugin {
+    background-color: rgba(0, 0, 0, 0);
+  }
+
+  label.match.description {
+    font-size: 10px;
+  }
+
+  label.plugin.info {
+    font-size: 14px;
+  }
+
+  .match {
+    background: transparent;
+  }
+
+  .match:selected {
+    border-left: 4px solid ${mkRGB colors.accent};
+    background: transparent;
+    animation: fade 0.1s linear;
+  }
+
+  @keyframes fade {
+    0% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
 ''
