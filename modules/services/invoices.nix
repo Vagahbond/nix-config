c8ebadeda5ba5ae67be6b8ad57ab2541b05d1d17
Yoni FIRROLONI <pro@yoni-firroloni.com>
2025-12-01 18:57:54 +1300

    dolibarr: install

--- modules/services/invoices.nix
+++ modules/services/invoices.nix
@@ -1,7 +1,23 @@
-{config, ...}: {
-  age.secrets.invoiceshelfEnv = {
-    file = ../../secrets/invoiceshelf_env.age;
+{
+  config,
+  storageLocation,
+  ...
+}: {
+  /*
+    age.secrets.dolibarr = {
+    file = ../../secrets/dolibarr.age;
     mode = "440";
+  };
+  */
+  environment.persistence.${storageLocation} = {
+    directories = [
+      {
+        directory = "/var/lib/dolibarr";
+        user = "dolibarr";
+        group = "dolibarr";
+        mode = "u=rwx,g=rx,o=";
+      }
+    ];
   };
 
   services = {
@@ -10,6 +26,20 @@
         forceSSL = true;
         enableACME = true;
       };
+      "accounting.vagahbond.com" = {
+        forceSSL = true;
+        enableACME = true;
+      };
+    };
+
+    dolibarr = {
+      enable = true;
+      domain = "accounting.vagahbond.com";
+      database = {
+        type = "postgresql";
+        createLocally = true;
+      };
+      nginx = {};
     };
 
     kimai.sites."invoices.vagahbond.com" = {
