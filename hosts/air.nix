73defa71d93254678181aa04ea90e9cfdc910681
Yoni Firroloni <pro@yoni-firroloni.com>
2026-02-01 12:44:05 +1300

    build:fix

--- hosts/air.nix
+++ hosts/air.nix
@@ -2,11 +2,23 @@
   name = "air";
   platform = "aarch64-darwin";
 
-  configuration =
-    { username }:
-    {
-      system.stateVersion = 6;
-      users.users.${username}.hashedPassword =
-        "$y$j9T$ofYLQRbiSsTERtHKAoi.J1$XW1xU541EsKvdMc3WNMEliNvUn4tVxKl99PbSB5gUg/";
+  configuration = _: {
+    system.stateVersion = 6;
+    #  users.users.${username}.hashedPassword =
+    #       "$y$j9T$ofYLQRbiSsTERtHKAoi.J1$XW1xU541EsKvdMc3WNMEliNvUn4tVxKl99PbSB5gUg/";
+
+    shell = {
+      shellAliases = {
+        rebuild-remote = ''
+          f() {  \
+                   NIX_SSHOPTS="-i ~/.ssh/$1_access " \
+                   nixos-rebuild switch --sudo --ask-sudo-password \
+                   --flake .#"$1" --target-host $1 \
+                   --build-host $1 --show-trace \
+                   }; f\
+        '';
+      };
     };
+
+  };
 }
