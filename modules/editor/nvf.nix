fc431371270a885cfcf80bc11ce2efd25c4baeb9
Yoni Firroloni <pro@yoni-firroloni.com>
2026-02-12 19:10:01 +1300

    nvf: use their own nixpkgs

--- modules/editor/nvf.nix
+++ modules/editor/nvf.nix
@@ -14,8 +14,8 @@
     let
       nvf =
         (inputs.nvf.lib.neovimConfiguration {
-          inherit pkgs;
-          # pkgs = inputs.nvf.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.system};
+          # inherit pkgs;
+          pkgs = inputs.nvf.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.system};
           modules = [
             (
               { pkgs, ... }:
