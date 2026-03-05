f1a47e5817bcea0d0b62f0395115f7a207fe7966
Yoni Firroloni <pro@yoni-firroloni.com>
2026-03-06 12:21:24 +1300

    filestash: un-manage configuration (configure manually and then nixify)

--- modules/services/cloud.nix
+++ modules/services/cloud.nix
@@ -71,24 +71,26 @@
       ###################################################
       services.filestash = {
         enable = true;
-        settings = {
-          general = {
-            host = "cloud.vagahbond.com";
-            port = 8334;
-            secret_key_file = config.age.secrets.filestashSecretKey.path;
-            editor = "base";
-            fork_button = false;
-            upload_button = true;
-            filepage_default_view = "list";
-            filepage_default_sort = "type";
+        /*
+          settings = {
+            general = {
+              host = "cloud.vagahbond.com";
+              port = 8334;
+              secret_key_file = config.age.secrets.filestashSecretKey.path;
+              editor = "base";
+              fork_button = false;
+              upload_button = true;
+              filepage_default_view = "list";
+              filepage_default_sort = "type";
+            };
+            share.default_access = "viewer";
+            features.api.enable = false;
+            auth = {
+              admin = "$2a$12$6IeDB6RsGpntHVYqcADhteEwA7z/1AbUG1oj16Pkq9ibMSZP7tr06";
+            };
+            connections = [ ];
           };
-          share.default_access = "viewer";
-          features.api.enable = false;
-          auth = {
-            admin = "$2a$12$6IeDB6RsGpntHVYqcADhteEwA7z/1AbUG1oj16Pkq9ibMSZP7tr06";
-          };
-          connections = [ ];
-        };
+        */
       };
     };
 }
