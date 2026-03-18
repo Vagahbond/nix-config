5c46f972c63111cb7a4df6fdeb74714da22a32ca
Yoni Firroloni <pro@yoni-firroloni.com>
2026-03-18 15:01:44 +1300

    font: add mononoki

--- modules/terminal/shell.nix
+++ modules/terminal/shell.nix
@@ -5,35 +5,36 @@
     "framework"
   ];
 
-  sharedConfiguration = {
-    pkgs,
-    config,
-    ...
-  }: {
-    options = {
-      shell.shellAliases = pkgs.lib.mkOption {
-        type = pkgs.lib.types.attrs;
-        default = {};
-        description = ''
-          Specific aliases for that system.
-        '';
-        example = {
-          build = "sudo nixos-rebuild build";
+  sharedConfiguration =
+    {
+      pkgs,
+      config,
+      ...
+    }:
+    {
+      options = {
+        shell.shellAliases = pkgs.lib.mkOption {
+          type = pkgs.lib.types.attrs;
+          default = { };
+          description = ''
+            Specific aliases for that system.
+          '';
+          example = {
+            build = "sudo nixos-rebuild build";
+          };
         };
       };
-    };
-    config = {
-      # Just basic tools I like to use
-      environment.systemPackages = with pkgs; [
-        lsd
-        fzf
-        ripgrep
-        yazi
-        fd
-      ];
+      config = {
+        # Just basic tools I like to use
+        environment.systemPackages = with pkgs; [
+          lsd
+          fzf
+          ripgrep
+          yazi
+          fd
+        ];
 
-      environment.shellAliases =
-        {
+        environment.shellAliases = {
           build-iso-remote = "nix build github:vagahbond/nix-config#nixosConfigurations.live.config.system.build.isoImage";
           build-iso = "nix build .#nixosConfigurations.live.config.system.build.isoImage";
           run = "f() {nix run nixpkgs#$1}; f";
@@ -51,48 +52,55 @@
           unzip-all = "for file in ./* ;do unzip -d \"\${file%.*}\" $file; done";
         }
         // config.shell.shellAliases;
+      };
     };
-  };
 
-  darwinConfiguration = {
-    username,
-    pkgs,
-    ...
-  }: {
-    fonts.packages = [pkgs.nerd-fonts.departure-mono];
+  darwinConfiguration =
+    {
+      username,
+      pkgs,
+      ...
+    }:
+    {
+      fonts.packages = [
+        pkgs.nerd-fonts.departure-mono
+        pkgs.nerd-fonts.mononoki
+      ];
 
-    home-manager.users.${username} = {
+      home-manager.users.${username} = {
+        programs.zsh = {
+          enable = true;
+          enableCompletion = true;
+          syntaxHighlighting.enable = true;
+        };
+      };
+    };
+
+  nixosConfiguration =
+    {
+      username,
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      users.defaultUserShell = pkgs.zsh;
+
       programs.zsh = {
         enable = true;
         enableCompletion = true;
         syntaxHighlighting.enable = true;
+
+        histSize = 10000;
       };
-    };
-  };
-
-  nixosConfiguration = {
-    username,
-    config,
-    pkgs,
-    ...
-  }: {
-    users.defaultUserShell = pkgs.zsh;
-
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      syntaxHighlighting.enable = true;
-
-      histSize = 10000;
-    };
-    environment = {
-      persistence.${config.persistence.storageLocation} = {
-        users.${username} = {
-          files = [
-            ".zshrc"
-          ];
+      environment = {
+        persistence.${config.persistence.storageLocation} = {
+          users.${username} = {
+            files = [
+              ".zshrc"
+            ];
+          };
         };
       };
     };
-  };
 }
