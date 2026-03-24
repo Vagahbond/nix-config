9d0dfdc53693f810eaf8ec2d23d715d5977c25c3
Yoni Firroloni <pro@yoni-firroloni.com>
2026-03-24 18:46:57 +1300

    opencloud: add webdav

--- modules/services/files.nix
+++ modules/services/files.nix
@@ -6,9 +6,6 @@
   nixosConfiguration =
     {
       config,
-      pkgs,
-      inputs,
-      self,
       ...
     }:
     {
@@ -38,22 +35,13 @@
             mode = "u=rwx,g=rx,o=";
           }
           {
-            directory = "/var/lib/filestash";
-            user = config.services.filestash.user;
-            group = config.services.filestash.group;
+            directory = "/var/lib/radicale";
+            user = "radicale";
+            group = "radicale";
             mode = "u=rwx,g=rx,o=";
           }
-
-          {
-            directory = "/var/cache/filestash";
-            user = config.services.filestash.user;
-            group = config.services.filestash.group;
-            mode = "u=rwx,g=rx,o=";
-          }
-
         ];
       };
-
 
       services = {
         nginx.virtualHosts = {
@@ -61,12 +49,43 @@
             forceSSL = true;
             enableACME = true;
 
-            locations."/" = {
-              extraConfig = ''
-                client_max_body_size 1G;
-              '';
-              proxyPass = "http://127.0.0.1:${toString config.services.opencloud.port}";
-              proxyWebsockets = true; # needed if you need to use WebSocket
+            locations = {
+              "/" = {
+                extraConfig = ''
+                  client_max_body_size 1G;
+                '';
+                proxyPass = "http://127.0.0.1:${toString config.services.opencloud.port}";
+                proxyWebsockets = true; # needed if you need to use WebSocket
+              };
+              # Radicale endpoints for CalDAV and CardDAV
+              "/caldav/" = {
+                proxyPass = "http://127.0.0.1:5232";
+                extraConfig = "
+            proxy_set_header X-Remote-User $remote_user; # provide username to CalDAV
+            proxy_set_header X-Script-Name /caldav;
+          ";
+              };
+              "/.well-known/caldav" = {
+                proxyPass = "http://127.0.0.1:5232";
+                extraConfig = "
+            proxy_set_header X-Remote-User $remote_user; # provide username to CalDAV
+            proxy_set_header X-Script-Name /caldav;
+          ";
+              };
+              "/carddav/" = {
+                proxyPass = "http://127.0.0.1:5232";
+                extraConfig = "
+            proxy_set_header X-Remote-User $remote_user; # provide username to CardDAV
+            proxy_set_header X-Script-Name /carddav;
+          ";
+              };
+              "/.well-known/carddav/" = {
+                proxyPass = "http://127.0.0.1:5232";
+                extraConfig = "
+            proxy_set_header X-Remote-User $remote_user; # provide username to CardDAV
+            proxy_set_header X-Script-Name /carddav;
+          ";
+              };
             };
 
           };
@@ -78,162 +97,33 @@
           environmentFile = config.age.secrets.opencloudEnv.path;
           port = 9208;
         };
-      };
 
-      imports = [ inputs.filestash.nixosModules.default ];
-
-      ###################################################################
-      # IMPERMANENCE                                                    #
-      ###################################################################
-
-      ###################################################
-      # SECRETS                                         #
-      ###################################################
-      age.secrets = {
-        filestashSecretKey = {
-          file = ../../secrets/filestash_secret_key.age;
-          mode = "440";
-          owner = config.services.filestash.user;
-          group = config.services.filestash.group;
+        radicale = {
+          enable = true;
+          settings = {
+            server = {
+              hosts = [ "127.0.0.1:5232" ];
+              ssl = false; # disable SSL, only use when behind reverse proxy
+            };
+            auth = {
+              type = "http_x_remote_user"; # disable authentication, and use the username that OpenCloud provides is
+            };
+            web = {
+              type = "none";
+            };
+            storage = {
+              filesystem_folder = "/var/lib/radicale/collections";
+            };
+            # logging = {
+            #   level = "debug"; # optional, enable debug logging
+            #   bad_put_request_content = true; # only if level=debug
+            #   request_header_on_debug = true; # only if level=debug
+            #   request_content_on_debug = true; # only if level=debug
+            #   response_content_on_debug = true; # only if level=debug
+            # };
+          };
         };
-        # filestashApiKey = {
-        #   file = ../../secrets/filestash_api_key.age;
-        #   mode = "440";
-        #   owner = config.services.filestash.user;
-        #   group = config.services.filestash.group;
-        # };
-
       };
 
-      ###################################################
-      # SSL                                             #
-      ###################################################
-
-      # services.nginx.virtualHosts = {
-      #   "files.vagahbond.com" = {
-      #     forceSSL = true;
-      #     enableACME = true;
-      #
-      #     locations."/" = {
-      #       extraConfig = ''
-      #         client_max_body_size 100M;
-      #       '';
-      #       proxyPass = "http://127.0.0.1:8334";
-      #       proxyWebsockets = true; # needed if you need to use WebSocket
-      #     };
-      #
-      #   };
-      # };
-
-      ###################################################
-      # SERVICES                                        #
-      ###################################################
-      services.filestash = {
-        enable = true;
-
-        paths = {
-          config = "/var/lib/filestash/config.json";
-        };
-
-        # Cannot really make reproducible without heavily improving the nixos module
-
-        #   settings = {
-        #     general = {
-        #       secret_key_file = config.age.secrets.filestashSecretKey.path;
-        #       name = "La boite à fichiers";
-        #       port = 8334;
-        #       host = "files.vagahbond.com";
-        #       force_ssl = true;
-        #       editor = "base";
-        #       fork_button = null;
-        #       logout = "https://rickastley.co.uk/";
-        #       display_hidden = true;
-        #       refresh_after_upload = true;
-        #       upload_button = true;
-        #       upload_pool_size = null;
-        #       upload_chunk_size = null;
-        #       filepage_default_view = null;
-        #       filepage_default_sort = null;
-        #       cookie_timeout = null;
-        #       custom_css = null;
-        #     };
-        #     features = {
-        #       api = {
-        #         enable = null;
-        #         api_key_file = config.age.secrets.filestashApiKey.path;
-        #       };
-        #       share = {
-        #         enable = null;
-        #         default_access = null;
-        #         redirect = null;
-        #       };
-        #       protection = {
-        #         iframe = null;
-        #         enable_chromecast = false;
-        #         signature = null;
-        #         disable_svg = null;
-        #         zip_timeout = null;
-        #         disable_csp = null;
-        #       };
-        #       mcp = {
-        #         enable = null;
-        #         can_edit = null;
-        #       };
-        #       server = {
-        #         console_enable = false;
-        #       };
-        #       search = {
-        #         explore_timeout = null;
-        #       };
-        #       office = {
-        #         office_server = "http://[::1]:9980";
-        #         filestash_server = "https://files.vagahbond.com";
-        #         rewrite_discovery_url = null;
-        #         enable = true;
-        #       };
-        #       video = {
-        #         blacklist_format = null;
-        #         enable_transcoder = true;
-        #       };
-        #     };
-        #     log = {
-        #       enable = null;
-        #       level = null;
-        #       telemetry = true;
-        #     };
-        #     email = {
-        #       server = null;
-        #       port = null;
-        #       username = null;
-        #       password = null;
-        #       from = null;
-        #     };
-        #     auth = {
-        #       admin = "$2a$10$8Y0hz7hUrBpIqVO6XazpBup7g4y9yRk6KfV75kqrLhPyqISAOjKse";
-        #     };
-        #     middleware = {
-        #       identity_provider = {
-        #         type = "local";
-        #         params = "DlwdmFvZHBh8hDgFI53TjjqYZSgjUawhX-v3voJtojeueSt2PJSZCHRw-DZTJ3EENY0nh-rM3k-Gbr20JiMzCfjTTfXrJr4AhxQxIVqAY1AJG_U28sr5zEP0JYZEps8lmk83T9AorPOCWHJo5j2LN0vaJkHZEx0Lt8yArPN9AKti4-TZwpM_hxjMvsuU8GNAUEA2dhW19XLCt3-uXYCYbPLh2Zp4zLcu8kPjmPP-XLzX4azylbGFVTWD1-r4IHnGtf9L-Ejw0sOrKeOKIdfxV1h6tjlzBbzvjycAvP7op55jT39eX40R9yNpyWWtiG8177uLrNDrdSkLq6WqkY0PffxEpy6hzn2syRWZA0LaFX9jXDRSxOEn82_1YDdLrqTK4XQiMgw=";
-        #       };
-        #       attribute_mapping = {
-        #         related_backend = "AWS";
-        #         params = "DlwdmFvZHBh8hDgGjEaYoEKuJFQfI1OGzjBDmTXKuYOoTJrVP6FBOsXnS9DB2963AuAp3xIRLnOnN9LWP9VAyiWWYhIDV3DaQHCXakmAACZKL66R6YlQ6okodDXDPY1gLDajJrkYSpXtGpEOpWB4S8Mjv8SxxOalEycUTdEOp0pSBboGAXaf9dk-4-vnFDHzFR8RTNUjO7355-D9IymSrmOIsHXSiC8hgBwNCmsS2bA2vtwy0EvydTW9OB_YiK-KVXgpXwIaAbCOL3aAwiuw2kcLtE5T0MOqx9k2BSEQ9uOEeBUvEZyw3A0pVHuSHmTW5zc8";
-        #       };
-        #     };
-        #     constant = {
-        #       user = "filestash";
-        #       license = "agpl";
-        #     };
-        #     # To edit, copy config file to /var/lib/filestash/config.json and edit from admin panel
-        #     connections = [
-        #       {
-        #         type = "s3";
-        #         label = "AWS";
-        #       }
-        #     ];
-        #
-        #   };
-      };
     };
 }
