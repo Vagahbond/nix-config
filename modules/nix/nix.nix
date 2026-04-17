5ca973ff0d8a8f346f62398db34f567ece71f49b
Vagahbond <vagahbond@pm.me>
2026-04-17 12:48:31 +1200

    automation: move build dir before build

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -29,7 +29,7 @@
         settings = {
 
           substituters = [ "https://cache.nixos.org/" ];
-          # trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+          trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
           experimental-features = [
             "nix-command"
             "flakes"
@@ -37,6 +37,9 @@
           trusted-users = [
             "root"
           ];
+
+          buildDir = "/nix/build";
+
         };
 
         gc = {
