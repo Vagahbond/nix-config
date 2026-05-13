4235bde3000b8fd987c16f46d29c3b311eabb0d2
Vagahbond <vagahbond@pm.me>
2026-05-13 15:55:07 +1200

    proxy: remove metrics

--- modules/services/proxy.nix
+++ modules/services/proxy.nix
@@ -16,21 +16,6 @@
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
 
-      commonHttpConfig = ''
-        vhost_traffic_status_zone;
-        vhost_traffic_status_filter_by_set_key $host zone=vhost;
-      '';
-
-      virtualHosts.localhost = {
-        locations = {
-          "/metrics" = {
-            extraConfig = ''
-              vhost_traffic_status_display;
-              vhost_traffic_status_display_format prometheus;
-            '';
-          };
-        };
-      };
     };
     networking.firewall.allowedTCPPorts = [
       443
