1be1bddd8134cc3a52e2f5833149de54deb884ab
Vagahbond <vagahbond@pm.me>
2026-06-05 14:46:13 +1200

    pixel: curate modules

--- hosts/pixel.nix
+++ hosts/pixel.nix
@@ -9,56 +9,22 @@
     ];
     editor = [
       "nvf"
-      "office"
     ];
     network = [
       "ssh"
-      "vpn"
-      "wifi"
     ];
     nix = [
-      "live"
       "nix"
       "remoteBuild"
     ];
     security = [
-      "fingerprint"
-      "keyring"
       "secrets"
-    ];
-    services = [
-      "uptime"
-      "analytics"
-      "budget"
-      "blog"
-      "automation"
-      "photos"
-      "builder"
-      "files"
-      "homepage"
-      "audio-experiments"
-      "invoices"
-      "metrics"
-      "mkReset"
-      "notes"
-      "office"
-      "pdf"
-      "postgres"
-      "proxy"
-      "ssh"
-      "vaultwarden"
     ];
     terminal = [
       "prompt"
       "shell"
       "rss"
     ];
-    virtualization = [
-      "docker"
-      "kubernetes"
-      "libvirt"
-    ];
-    impermanence = { };
     locales = { };
     system = { };
     user = { };
